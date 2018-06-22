af8c29b85bda44df2d53d7f9790ae1ed9ea54f26
Laurence Ollagnier <ollagnie2u@a-mn-p322-pc13.artem.site.univ-lorraine.fr>
2018-06-22 18:35:03 +0200

    modifs de la classe ascenseur

--- ascenseur.h
+++ ascenseur.h
@@ -1,10 +1,15 @@
 #ifndef ASCENSEUR_H
 #define ASCENSEUR_H
 
+//classe ascenseur
 typedef struct Ascenseur ascenseur;
 struct Ascenseur{
 	int nbFloor;
-};
+	int nbPersonne;
+	int capacite;
+	int etageActuel;
+	enum status={up=1, down=-1, stopped=0};
+}
 
 void DisplayAscenseur(ascenseur a);
 #endif
